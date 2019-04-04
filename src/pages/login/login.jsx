303d7575408a5ff651cb5ecfa0cd424d4d1b5109
zxfjd3g <258147149@qq.com>
2019-04-04 11:34:23 +0800

    login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -18,11 +18,23 @@ export default class Login extends Component {
   // login = (name, pwd) => {
   login = async ({username, password}) => {
     // alert(`发送ajax请求: username=${username}, password=${password}`)
-    const result = await reqLogin(username, password)
-    console.log('result', result)
+    const result = await reqLogin(username, password) // {status: 0, data: user对象} {status: 1, msg: '错误信息'}
+    // console.log('result', result)
+    if(result.status===0) { // 成功了
+
+      // 跳转到后台管理界面
+      this.props.history.replace('/')
+    } else {
+      // 显示错误信息
+      this.setState({
+        errorMsg: result.msg
+      })
+    }
   }
 
   render() {
+    const {errorMsg} = this.state
+
     return (
       <div className='login'>
         <div className='login-header'>
@@ -31,7 +43,9 @@ export default class Login extends Component {
         </div>
         <div className='login-content'>
           <div className='login-box'>
-            <div className="error-msg-wrap"></div>
+            <div className="error-msg-wrap">
+              <div className={errorMsg ? 'show' : ''}>{errorMsg}</div>
+            </div>
             <div className="title">用户登陆</div>
             <LoginForm login={this.login}/>
           </div>
