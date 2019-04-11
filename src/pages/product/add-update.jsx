c2b53c86ca8f8c02723cf04630e254a99ffbafe5
zxfjd3g <258147149@qq.com>
2019-04-11 12:07:20 +0800

    image

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -12,38 +12,111 @@ import LinkButton from '../../components/link-button'
 import PictureWall from './picture-wall'
 import RichTextEditor from './rich-text-editor'
 
-const {Item} = Form
+import {reqCategories} from '../../api'
 
-const options = [{
-  value: 'zhejiang',
-  label: 'Zhejiang',
-  children: [{
-    value: 'hangzhou',
-    label: 'Hangzhou',
-    children: [{
-      value: 'xihu',
-      label: 'West Lake',
-    }],
-  }],
-}, {
-  value: 'jiangsu',
-  label: 'Jiangsu',
-  children: [{
-    value: 'nanjing',
-    label: 'Nanjing',
-    children: [{
-      value: 'zhonghuamen',
-      label: 'Zhong Hua Men',
-    }],
-  }],
-}];
+const {Item} = Form
 
 /*
 商品添加/更新的路由组件
  */
 class ProductAddUpdate extends Component {
+
+  state = {
+    options: [], // 用来显示级联列表的数组
+  }
+
+  /*
+  选择某个分类项时的回调
+  加载对应的二级分类显示
+   */
+  loadData = async (selectedOptions) => {
+    console.log('loadDate()', selectedOptions)
+    const targetOption = selectedOptions[selectedOptions.length - 1]
+    targetOption.loading = true // 显示loading
+
+    // 异步请求获取对应的二级分类列表
+    await this.getCategories(targetOption.value) // await的作用: 保证完成执行完保存的分类数组才进入后面的语句
+    const subCategories = this.subCategories
+    targetOption.loading = false // 隐藏loading
+    if(subCategories && subCategories.length>0) { // 有子分类
+      // 生成一个二级的options
+      const cOptions = subCategories.map(c => ({
+        value: c._id,
+        label: c.name,
+        isLeaf: true,
+      }))
+      // 一级的options
+      const {options} = this.state
+
+      // 添加为对应的option的children(子options)
+      targetOption.children = cOptions
+    } else { // 没有子分类
+      targetOption.isLeaf = true
+    }
+
+    // 更新options状态
+    this.setState({
+      options: [...this.state.options],
+    });
+
+  }
+
+  /*
+  获取指定分类id的子分类列表
+  如果parentId为0时获取一级列表
+   */
+  getCategories = async (parentId) => {
+    const result = await reqCategories(parentId)
+    if(result.status===0) {
+      const categories = result.data
+      if(parentId==='0') {
+        // 保存一级分类列表
+        this.categories = categories
+        this.initOptions()
+      } else {
+        // 保存分类列表
+        this.subCategories = categories
+      }
+    }
+  }
+
+  /*
+  生成级联的一级列表
+   */
+  initOptions = () => {
+    // 根据一级分类数组生成option的数组
+    const options = this.categories.map(c => ({
+      value: c._id,
+      label: c.name,
+      isLeaf: false,
+    }))
+    // 更新状态
+    this.setState({
+      options
+    })
+  }
+
+  // 添加/更新
+  submit = () => {
+    // 收集产品相关信息
+    const values = this.props.form.getFieldsValue()
+
+    /*
+    在父组件中得到子组件对象: <Child ref='xxx'>  this.refs.xxx
+    调用子组件对象的方法: this.refs.xxx.fn()
+     */
+    const imags = this.refs.pw.getImgs()
+    console.log(values, imags)
+  }
+
+  componentDidMount () {
+    // 异步获取一级分类列表
+    this.getCategories('0')
+  }
+
   render() {
 
+    const {options} = this.state
     const {getFieldDecorator} = this.props.form
 
     const title = (
@@ -97,12 +170,15 @@ class ProductAddUpdate extends Component {
               getFieldDecorator('categories', {
                 initialValue: [],
               })(
-                <Cascader options={options} placeholder="Please select" />
+                <Cascader
+                  options={options}
+                  loadData={this.loadData}
+                />
               )
             }
           </Item>
           <Item label="商品图片" {...formItemLayout}>
-            <PictureWall/>
+            <PictureWall ref='pw'/>
           </Item>
           <Item
             label="商品详情"
@@ -110,7 +186,7 @@ class ProductAddUpdate extends Component {
             wrapperCol={{ span: 20 }}>
             <RichTextEditor />
           </Item>
-          <Button type='primary'>提交</Button>
+          <Button type='primary' onClick={this.submit}>提交</Button>
         </Form>
       </Card>
     )
--- src/pages/product/picture-wall.jsx
+++ src/pages/product/picture-wall.jsx
@@ -1,28 +1,53 @@
 import React, {Component} from 'react'
 import { Upload, Icon, Modal } from 'antd';
 
+/*
+操作商品图片的照片墙组件
+ */
 export default class PicturesWall extends Component {
   state = {
-    previewVisible: false,
-    previewImage: '',
-    fileList: [{
-      uid: '-1',
-      name: 'xxx.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    }],
+    previewVisible: false, // 是否显示大图预览
+    previewImage: '', // 可以预览的大图url
+    // file: 代表一个上传文件的相关信息的对象
+    fileList: [],
   };
 
+  // 获取所有已上传图片文件名的数组
+  getImgs = () => {
+    return this.state.fileList.map(file => file.name)
+  }
+
+  // 取消大图预览(隐藏Modal)
   handleCancel = () => this.setState({ previewVisible: false })
 
   handlePreview = (file) => {
+    console.log('handlePreview', file)
     this.setState({
-      previewImage: file.url || file.thumbUrl,
+      previewImage: file.url,
       previewVisible: true,
     });
   }
 
-  handleChange = ({ fileList }) => this.setState({ fileList })
+  /*
+  文件状态改变的监听回调
+    上传中、完成、失败, 删除
+  */
+  handleChange = ({ fileList }) => {
+
+    // 取出上传的file
+    const file = fileList[fileList.length-1]
+    console.log('handleChange()', file.status, file)
+    if(file.status==='done') { // 上传文件成功
+      // 将上传文件的文件名和url保存到file上
+      const {name, url} = file.response.data
+      file.name = name
+      file.url = url
+    }
+
+    this.setState({
+      fileList
+    })
+  }
 
   render() {
     const { previewVisible, previewImage, fileList } = this.state;
@@ -32,17 +57,29 @@ export default class PicturesWall extends Component {
         <div className="ant-upload-text">Upload</div>
       </div>
     );
+
     return (
-      <div className="clearfix">
+      <div>
+        {/*
+        action: 指定上传的后台处理url
+        listType: 显示的样式效果
+        fileList: 所有上传图片的相关信息对象的数组
+        name: 请求参数的参数名
+        onPreview: 点击预览的回调
+        onChange:
+        */}
         <Upload
-          action="//jsonplaceholder.typicode.com/posts/"
+          action="/manage/img/upload"
           listType="picture-card"
           fileList={fileList}
+          name='image'
           onPreview={this.handlePreview}
           onChange={this.handleChange}
         >
           {fileList.length >= 3 ? null : uploadButton}
         </Upload>
+
+
         <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
           <img alt="example" style={{ width: '100%' }} src={previewImage} />
         </Modal>
